8d2c4a8cd8c94ad0b8ed570b649e1527b3499cd9
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-09-22 10:29:51 +0700

    reset password

--- resources/views/pages/mahasiswa/actions.blade.php
+++ resources/views/pages/mahasiswa/actions.blade.php
@@ -1,13 +1,13 @@
 <div class="btn-list">
-    <button class="btn btn-sm btn-secondary bg-secondary-gradient m-0" data-bs-toggle="modal"
-        data-bs-target="#extralargemodal-{{ $mahasiswa->user_mahasiswa->id }}">Semester</button>
-    @include('pages.mahasiswa.modal')
     <form action="{{ route('admin.reset_password', $mahasiswa->id) }}" method="POST">
         @csrf
         <button type="submit" class="btn btn-sm btn-warning" onclick="return confirm('Are you sure?')">
             Reset Password
         </button>
     </form>
+    <button class="btn btn-sm btn-secondary bg-secondary-gradient m-0" data-bs-toggle="modal"
+        data-bs-target="#extralargemodal-{{ $mahasiswa->user_mahasiswa->id }}">Semester</button>
+    @include('pages.mahasiswa.modal')
     <form action="{{ route('mahasiswa.destroy', $mahasiswa->id) }}" method="POST">
         <a class="btn btn-sm btn-primary" href="{{ route('mahasiswa.edit', $mahasiswa->id) }}">
             <span class="fe fe-edit"> </span>
