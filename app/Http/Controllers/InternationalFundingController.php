d67c25bcb680e54d74bf203b48106191207b4daa
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-09-22 11:22:50 +0700

    international funding

---
+++ app/Http/Controllers/InternationalFundingController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\InternationalFunding;
+use Illuminate\Http\Request;
+
+class InternationalFundingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\InternationalFunding  $internationalFunding
+     * @return \Illuminate\Http\Response
+     */
+    public function show(InternationalFunding $internationalFunding)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\InternationalFunding  $internationalFunding
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(InternationalFunding $internationalFunding)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\InternationalFunding  $internationalFunding
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, InternationalFunding $internationalFunding)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\InternationalFunding  $internationalFunding
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(InternationalFunding $internationalFunding)
+    {
+        //
+    }
+}
---
+++ app/Models/InternationalFunding.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InternationalFunding extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name'];
+}
