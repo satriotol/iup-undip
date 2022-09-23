dbdbf3fba0e71ac9504fa7366565384caf36f145
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-09-23 08:57:06 +0700

    fixing

--- resources/views/pages/mahasiswa/index.blade.php
+++ resources/views/pages/mahasiswa/index.blade.php
@@ -31,7 +31,7 @@
                                     <th>HP</th>
                                     <th>Fakultas / Batch</th>
                                     <th>Asal</th>
-                                    {{-- <th>Semester</th> --}}
+                                    <th>Semester</th>
                                     <th>Action</th>
                                 </tr>
                             </thead>
@@ -45,11 +45,14 @@
                                         <td>{{ $mahasiswa->user_mahasiswa->phone }}</td>
                                         <td>{{ $mahasiswa->user_mahasiswa->major->name }}/{{ $mahasiswa->user_mahasiswa->batch->year }}
                                         <td>{{ $mahasiswa->user_mahasiswa->country->name }}
-                                        {{-- <td style="background-color: {{ $mahasiswa->user_mahasiswa->mahasiswa_semesters->first()->semester_status->color }}">{{ $mahasiswa->user_mahasiswa->mahasiswa_semesters->first()->semester->year }} -
-                                            {{ $mahasiswa->user_mahasiswa->mahasiswa_semesters->first()->semester->semester }}
+                                        <td
+                                            style="background-color: {{ $mahasiswa->user_mahasiswa->mahasiswa_semesters->first()->semester_status->color ?? '' }}">
+                                            {{ $mahasiswa->user_mahasiswa->mahasiswa_semesters->first()->semester->year ?? '' }}
+                                            -
+                                            {{ $mahasiswa->user_mahasiswa->mahasiswa_semesters->first()->semester->semester ?? '' }}
                                             /
-                                            {{ $mahasiswa->user_mahasiswa->mahasiswa_semesters->first()->semester_status->name }}
-                                        </td> --}}
+                                            {{ $mahasiswa->user_mahasiswa->mahasiswa_semesters->first()->semester_status->name ?? '' }}
+                                        </td>
                                         <td name="bstable-actions">
                                             @include('pages.mahasiswa.actions')
                                         </td>
