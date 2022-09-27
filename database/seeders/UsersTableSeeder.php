15f990d001ba7304782f55e72678df3fd97d5f05
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-09-27 21:00:30 +0700

    fixing

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -29,17 +29,43 @@ class UsersTableSeeder extends Seeder
                 'remember_token' => NULL,
                 'created_at' => '2022-09-21 14:42:09',
                 'updated_at' => '2022-09-22 01:14:21',
+                'photo' => NULL,
             ),
             1 => 
             array (
                 'id' => 9,
-                'name' => 'satrio',
+                'name' => 'Satrio',
                 'email' => 'satriotol23@gmaiwda.co',
                 'email_verified_at' => NULL,
                 'password' => '$2y$10$2x9UdM5gv5UafIdNMw.m5uyOUu3.WGxKGr5qVOgFo4EhClWx6ODNi',
                 'remember_token' => NULL,
                 'created_at' => '2022-09-22 03:18:10',
-                'updated_at' => '2022-09-22 03:29:14',
+                'updated_at' => '2022-09-26 14:27:34',
+                'photo' => 'file/09262022142734-Profile 2.png',
+            ),
+            2 => 
+            array (
+                'id' => 10,
+                'name' => 'satriotol',
+                'email' => 'otol@gmail.com',
+                'email_verified_at' => NULL,
+                'password' => '$2y$10$bzWa1Rx8nkWZR3.Uh4B4megww4GvHYZLdMHzo6irC11BtihzMaGG2',
+                'remember_token' => NULL,
+                'created_at' => '2022-09-24 18:05:20',
+                'updated_at' => '2022-09-25 13:56:41',
+                'photo' => 'file/09252022135641-196405162000121002.bmp',
+            ),
+            3 => 
+            array (
+                'id' => 11,
+                'name' => 'aaa',
+                'email' => 'admin@admin.com',
+                'email_verified_at' => NULL,
+                'password' => '',
+                'remember_token' => NULL,
+                'created_at' => '2022-09-26 13:09:53',
+                'updated_at' => '2022-09-27 13:59:59',
+                'photo' => NULL,
             ),
         ));
         
