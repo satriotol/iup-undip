4848fbcc067765b0448f457771db3a92a6044762
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2023-01-28 23:04:45 +0700

    fixing login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,8 @@
     <title>Login</title>
 
     <!-- BOOTSTRAP CSS -->
-    <link id="style" href="{{ asset('assets/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" />
+    <link id="style" href="{{ asset('assets/plugins/bootstrap/css/bootstrap.min.css') }}"
+        rel="stylesheet" />
 
     <!-- STYLE CSS -->
     <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" />
@@ -35,7 +36,7 @@
 <body class="app sidebar-mini ltr">
 
     <!-- BACKGROUND-IMAGE -->
-    <div class="login-img">
+    <div class="login-img" id="app">
 
         <!-- PAGE -->
         <div class="page">
@@ -51,48 +52,42 @@
 
                 <div class="container-login100">
                     <div class="wrap-login100 p-6">
-                        <form method="POST" action="{{ route('login.store') }}">
-                            @csrf
-
-                            <span class="login100-form-title pb-5">
-                                Login
-                            </span>
-                            @include('partials.errors')
-                            <div class="panel panel-primary">
-                                <div class="panel-body tabs-menu-body p-0 pt-5">
-                                    <div class="tab-content">
-                                        <div class="tab-pane active" id="tab5">
-                                            <div class="wrap-input100 validate-input input-group"
-                                                data-bs-validate="Valid email is required: ex@abc.xyz">
-                                                <a href="javascript:void(0)"
-                                                    class="input-group-text bg-white text-muted">
-                                                    <i class="zmdi zmdi-email text-muted" aria-hidden="true"></i>
-                                                </a>
-                                                <input class="input100 border-start-0 form-control ms-0" type="email"
-                                                    name="email" required placeholder="Email">
-                                            </div>
-                                            <div class="wrap-input100 validate-input input-group" id="Password-toggle">
-                                                <a href="javascript:void(0)"
-                                                    class="input-group-text bg-white text-muted">
-                                                    <i class="zmdi zmdi-eye text-muted" aria-hidden="true"></i>
-                                                </a>
-                                                <input class="input100 border-start-0 form-control ms-0" type="password"
-                                                    name="password" required placeholder="Password">
-                                            </div>
-                                            <div class="container-login100-form-btn">
-                                                <button type="submit"
-                                                    class="login100-form-btn btn-primary">Masuk</button>
-                                            </div>
-                                            <div class="text-center pt-3">
-                                                <p class="text-dark mb-0">Tidak Punya Akun ?<a href="{{route('register')}}"
-                                                        class="text-primary ms-1">Daftar</a></p>
-                                            </div>
+                        <span class="login100-form-title pb-5">
+                            Login
+                        </span>
+                        @include('partials.errors')
+                        <div class="panel panel-primary">
+                            <div class="panel-body tabs-menu-body p-0 pt-5">
+                                <div class="tab-content">
+                                    <div class="tab-pane active" id="tab5">
+                                        <div class="wrap-input100 validate-input input-group"
+                                            data-bs-validate="Valid email is required: ex@abc.xyz">
+                                            <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
+                                                <i class="zmdi zmdi-email text-muted" aria-hidden="true"></i>
+                                            </a>
+                                            <input class="input100 border-start-0 form-control ms-0" type="email"
+                                                v-model="form.email" name="email" required placeholder="Email">
+                                        </div>
+                                        <div class="wrap-input100 validate-input input-group" id="Password-toggle">
+                                            <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
+                                                <i class="zmdi zmdi-eye text-muted" aria-hidden="true"></i>
+                                            </a>
+                                            <input class="input100 border-start-0 form-control ms-0" type="password"
+                                                v-model="form.password" name="password" required placeholder="Password">
+                                        </div>
+                                        <div class="container-login100-form-btn">
+                                            <button type="submit" class="login100-form-btn btn-primary"
+                                                @click="login">Masuk</button>
+                                        </div>
+                                        <div class="text-center pt-3">
+                                            <p class="text-dark mb-0">Tidak Punya Akun ?<a
+                                                    href="{{ route('register') }}" class="text-primary ms-1">Daftar</a>
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-
-                        </form>
+                        </div>
                     </div>
                 </div>
                 <!-- CONTAINER CLOSED -->
@@ -121,8 +116,64 @@
     <script src="{{ asset('assets/js/themeColors.js') }}"></script>
 
     <!-- CUSTOM JS -->
-    {{-- <script src="{{ asset('assets/js/custom.js') }}"></script> --}}
-
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.6.7/dist/sweetalert2.all.min.js"></script>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.1.3/axios.min.js"
+        integrity="sha512-0qU9M9jfqPw6FKkPafM3gy2CBAvUWnYVOfNPDYKVuRTel1PrciTj+a9P3loJB+j0QmN2Y0JYQmkBBS8W+mbezg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script>
+        const {
+            createApp
+        } = Vue
+        createApp({
+            data() {
+                return {
+                    message: 'Hello Vue!',
+                    form: {
+                        email: '',
+                        password: '',
+                    },
+                }
+            },
+            mounted() {
+            },
+            methods: {
+                login() {
+                    Swal.fire({
+                        title: 'Mencoba Masuk',
+                        icon: 'info',
+                        timerProgressBar: true,
+                        didOpen: () => {
+                            Swal.showLoading()
+                        },
+                        allowOutsideClick: false
+                    });
+                    // console.log(this.form);
+                    axios.post('/login', this.form)
+                        .then((res) => {
+                            console.log(res);
+                            Swal.fire({
+                                title: 'Sukses',
+                                icon: 'success',
+                                confirmButtonText: 'Lanjut',
+                            }).then((result) => {
+                                /* Read more about isConfirmed, isDenied below */
+                                if (result.isConfirmed) {
+                                    window.location.href = res.request.responseURL
+                                }
+                            })
+                        }).catch((err) => {
+                            Swal.fire({
+                                title: 'Error',
+                                icon: 'error',
+                                text: err.response.data.message,
+                                confirmButtonText: 'Ok',
+                            })
+                        });
+                },
+            },
+        }).mount('#app')
+    </script>
 </body>
 
 </html>
