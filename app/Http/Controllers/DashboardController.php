01a9a327adc8b465acec36449eaeaa8c452f0254
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2023-01-28 22:28:08 +0700

    fixing

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -72,7 +72,7 @@ class DashboardController extends Controller
     }
     public function index(Request $request)
     {
-        Notification::send(null, new SendPushNotification('$title', '$message', '$fcmTokens'));
+        Notification::send(null, new SendPushNotification('$title', '$message', 'cQ6rY016KhEpmXIG7clOp6:APA91bHPNkgRyQbMju09fnXKeULJR7zna0sIFm8l6o36Ly3v0OMs91Y3oiCrZC5gY-K4TfW-mTwSVsyMSlzb63j4UekaqPfWpRej8oNK4bWOMl_IeQeg0_Qbw7Ol8L2-RWl_QikIwDB4'));
         $semesterStatuses = SemesterStatus::all();
         $users = User::has('user_mahasiswa')->whereHas('user_mahasiswa', function ($q) use ($request) {
             $q->where('batch_id', $request->batch);
